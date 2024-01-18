770449f378526f5816bdac636014ead789ffaaf0
MutennRoshii <abdelkarimlamrini@outlook.com>
2024-01-18 02:43:06 +0100

    better keymaps

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -4,26 +4,40 @@ vim.g.maplocalleader = ' '
 
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 
--- My keymaps
-vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 vim.keymap.set('n', '<leader>pv', vim.cmd.Oil)
+
+-- [[ Quality of life ]]
+-- Center screen when jumping around
 vim.keymap.set('n', '<C-d>', '<C-d>zz')
 vim.keymap.set('n', '<C-u>', '<C-u>zz')
 vim.keymap.set('n', 'n', 'nzz')
 vim.keymap.set('n', 'N', 'Nzz')
-
--- Stay in indent mode
+-- Stay in indent mode when indenting
 vim.keymap.set("v", "<", "<gv", { noremap = true, silent = true })
 vim.keymap.set("v", ">", ">gv", { noremap = true, silent = true })
-
+vim.keymap.set("v", "=", "=gv", { noremap = true, silent = true })
+-- Don't replace the register when pasting
 vim.keymap.set("x", "p", [["_dP]])
-
 -- Remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+
+-- [[ Window management ]]
+vim.keymap.set("n", "<C-h>", "<C-w>h", { silent = true })
+vim.keymap.set("n", "<C-j>", "<C-w>j", { silent = true })
+vim.keymap.set("n", "<C-k>", "<C-w>k", { silent = true })
+vim.keymap.set("n", "<C-l>", "<C-w>l", { silent = true })
+
+-- [[ Buffer management ]]
+vim.keymap.set("n", "<leader>bd", "<cmd>bd<CR>", { silent = true })
+vim.keymap.set("n", "<leader>]n", "<cmd>bn<CR>", { silent = true })
+vim.keymap.set("n", "<leader>[p", "<cmd>bp<CR>", { silent = true })
 
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
+
+-- Code action keymaps
+vim.keymap.set("n", "<leader>cf", vim.lsp.buf.format)
